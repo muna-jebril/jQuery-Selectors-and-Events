dddf889b8cd24f179d641edea5943a72113d5038
muna-jebril <muna.jebril1995@hotmail.com>
2020-04-16 18:48:43 +0300

    fixing some error

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 'use strict';
-let templateId= "#horns-templte";
-Animals.all = [];
-let horns = [];
 
+Animals.all = [];
 
 function Animals(data) {
   this.image_url = data.image_url;
@@ -10,72 +8,37 @@ function Animals(data) {
   this.description = data.description;
   this.keyword = data.keyword;
   this.horns = data.horns;
-  
 }
-Animals.prototype.toHtml = function(){
+Animals.prototype.render = function () {
+  $('main').append('<div class="image-container"></div>');
 
-  let template= $('#horn-templte').html();
-  let html= Mustache.render(template,this);
-  $('main').append(html);
-  // return html;
-};
-$.get('data/page-1.json')
+  let $imageContainer = $('div[class="image-container"]');
+  $imageContainer.html($('#photo-template').html());
+  $imageContainer.find('h2').text(this.title);
+  $imageContainer.find('img').attr('src', this.image_url);
+  $imageContainer.find('p').text(this.description);
+  $imageContainer.attr('class', this.keyword);
+  $imageContainer.removeClass('image-container');
+
+}
+
+Animals.requestData = () => {
+
+  $.get('data/page-1.json')
     .then(data => {
       data.forEach(value => {
         Animals.all.push(new Animals(value));
-let item = new Animals (value);
-// console.log(item);
-item.toHtml();
 
-      // Animals.renderOption();
 
       });
-      
+      Animals.all.forEach(image => {
+        $('main').append(image.render());
 
-// .forEach(element => {
-
-//   var newObj = new Animals (element);
-//   horns.push(newObj);
-//   console.log(horns);
-// });
-// readJson.forEach(ourNewHornObj=>{
-//   var renderObj = ourNewHornObj.toHtml();
-//   $("#photo-template").append(renderObj);
-// });
-
-
-
-// }
-// Animals.prototype.render = function () {
-//   $('main').append('<div class="image-container"></div>');
-
-//   let $imageContainer = $('div[class="image-container"]');
-//   $imageContainer.html($('#photo-template').html());
-//   $imageContainer.find('h2').text(this.title);
-//   $imageContainer.find('img').attr('src', this.image_url);
-//   $imageContainer.find('p').text(this.description);
-//   $imageContainer.attr('class', this.keyword);
-//   $imageContainer.removeClass('image-container');
-
-// }
-
-// Animals.requestData = () => {
-
-//   $.get('data/page-1.json')
-//     .then(data => {
-//       data.forEach(value => {
-//         Animals.all.push(new Animals(value));
-
-
-//       });
-//       Animals.all.forEach(image => {
-//         $('main').append(image.render());
-
-//       })
-//       Animals.toHtml();
-//     })
-//     .then(Animals.filterSelected);
-// }
+      })
+      Animals.renderOption();
+    })
+    .then(Animals.filterSelected);
+}
 
 Animals.renderOption = () => {
 
@@ -88,8 +51,6 @@ Animals.renderOption = () => {
     }
   })
 }
-      Animals.renderOption();
-
 
 Animals.filterSelected = () => {
   $('select').on('change', function () {
@@ -109,5 +70,3 @@ Animals.filterSelected = () => {
 }
 
 Animals.requestData();
-
-    })
